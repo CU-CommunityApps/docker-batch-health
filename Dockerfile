5f234ef07d30d3bd90a667c039a08909dec126c1
Chad Bower <chadbower379@gmail.com>
2016-07-14 14:29:50 -0400

    first commit for batch health checker

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Pull base image.
+FROM dtr.cucloud.net/cs/awscli
+
+RUN apt-get update
+RUM apt-get install zlib1g-dev
+RUN gem install aws-sdk
+RUN gem install nokogiri
+RUN gem install json -v 1.8.3
+RUN gem install aws-sdk-v1
+
+COPY health-check.rb /opt/health-check.rb
+RUN chown daemon:daemon /opt/health-check.rb
+RUN chmod 775 /opt/health-check.rb
+
+USER daemon
+CMD /opt/health-check.rb 
---
+++ health-check.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'aws-sdk'
+require 'aws-sdk-v1'
+require 'json'
+
+unless ENV['batch_env']
+  puts "The environment variable batch_env has to be set"
+  exit 1
+end
+
+db =AWS::DynamoDB.new
+config_table = db.tables['batch_jobs'].load_schema
+node = config_table.items["#{ENV['batch_env']}"]
+
+if (File.exist?('/infra/work/staging/sys/batchContainer/stepRunByBatchStepRunner~batchContainerStep.runlock'))
+node.attributes.update do |u|
+              u.set(:status => "up")
+end
+else
+node.attributes.update do |u|
+              u.set(:status => "down")
+end
+
+end
